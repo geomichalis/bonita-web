c2978ca8d9691a036b1ee7d1221d0fb6aad5f0eb
ntith <nicolas.tith@bonitasoft.com>
2013-09-13 17:56:18 +0200

    fix popup in profile listing

--- console/console-war/src/main/java/org/bonitasoft/console/client/admin/profile/view/ProfileQuickDetailsPage.java
+++ console/console-war/src/main/java/org/bonitasoft/console/client/admin/profile/view/ProfileQuickDetailsPage.java
@@ -129,7 +129,7 @@ public class ProfileQuickDetailsPage extends AbstractProfileDetailsPage {
         @Override
         protected void setData(final String json, final Map<String, String> headers) {
             final ApiSearchResultPager resultPager = ApiSearchResultPager.parse(headers);
-            this.target.setText(String.valueOf(resultPager.getNbTotalResults()));
+            this.target.getElement().setInnerText(String.valueOf(resultPager.getNbTotalResults()));
         }
     }
 
@@ -156,7 +156,7 @@ public class ProfileQuickDetailsPage extends AbstractProfileDetailsPage {
         @Override
         protected void setData(final String json, final Map<String, String> headers) {
             final ApiSearchResultPager resultPager = ApiSearchResultPager.parse(headers);
-            this.target.setText(String.valueOf(resultPager.getNbTotalResults()));
+            this.target.getElement().setInnerText(String.valueOf(resultPager.getNbTotalResults()));
         }
     }
 
@@ -183,8 +183,7 @@ public class ProfileQuickDetailsPage extends AbstractProfileDetailsPage {
         @Override
         protected void setData(final String json, final Map<String, String> headers) {
             final ApiSearchResultPager resultPager = ApiSearchResultPager.parse(headers);
-            // this.target.setText(String.valueOf(resultPager.getNbTotalResults()));
-            // this.target.setText(String.valueOf(resultPager.getNbTotalResults()));
+            this.target.getElement().setInnerText(String.valueOf(resultPager.getNbTotalResults()));
         }
     }
 
@@ -211,7 +210,7 @@ public class ProfileQuickDetailsPage extends AbstractProfileDetailsPage {
         @Override
         protected void setData(final String json, final Map<String, String> headers) {
             final ApiSearchResultPager resultPager = ApiSearchResultPager.parse(headers);
-            this.target.setText(String.valueOf(resultPager.getNbTotalResults()));
+            this.target.getElement().setInnerText(String.valueOf(resultPager.getNbTotalResults()));
         }
     }
 }
