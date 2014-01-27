2d30fe567e9d8e522dddb52abfd999b25c725c18
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-01-27 16:52:40 +0100

    Enable SuperDevMode in a dev context. Fix page links in menu.

--- console/console-war/src/main/java/org/bonitasoft/console/client/menu/view/navigation/NavigationMenuView.java
+++ console/console-war/src/main/java/org/bonitasoft/console/client/menu/view/navigation/NavigationMenuView.java
@@ -113,7 +113,7 @@ public class NavigationMenuView extends RawView {
     private void getProfileEntries(String profileId, APICallback callback) {
         final Map<String, String> params = Collections.singletonMap(ProfileEntryItem.ATTRIBUTE_PROFILE_ID, profileId);
         ProfileEntryDefinition.get().getAPICaller()
-                .search(0, 100, ProfileEntryItem.ATTRIBUTE_INDEX + " " + "ASC", null, params, Arrays.asList(ProfileEntryItem.ATTRIBUTE_PAGE), callback);
+                .search(0, 100, ProfileEntryItem.ATTRIBUTE_INDEX + " " + "ASC", null, params, null/*Arrays.asList(ProfileEntryItem.ATTRIBUTE_PAGE)*/, callback);
     }
 
     private void listenViewChangeEvent(ChangeViewHandler handler) {
